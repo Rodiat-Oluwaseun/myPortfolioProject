04d6ed4353f2789fb4488d2cc6d11c5632615cc2
rodiat-oluwaseun <yemirolayemi@gmail.com>
2024-04-06 12:19:29 +0100

    ammendedment made and fontawesome library installed

--- src/assets/components/Project.jsx
+++ src/assets/components/Project.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Horiseon from "../../assets/Images/Horiseon.png";
 import WeatherDashboard from "../../assets/Images/WeatherDashboard.jpeg";
 import Book from "../../assets/Images/BookLibrary.jpeg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function Project() {
   return (
@@ -40,7 +41,7 @@ function Project() {
           {/* Weather project */}
           <div
             style={{ background: `url(${Horiseon})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto work-div object-cover h-48 w-96 ..."
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto  work-div  "
           >
             {/* Hover effect */}
             <div className="opacity-0 group-hover:opacity-100">
@@ -48,7 +49,7 @@ function Project() {
                 React JS Application
               </span>
               <div>
-                <a href="">
+                <a href="https://github.com/Rodiat-Oluwaseun">
                   <button> Demo</button>
                 </a>
                 <a href="">
@@ -59,8 +60,8 @@ function Project() {
           </div>
 
           <div
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto h-screen bg-coverA bg-center work-div"
             style={{ background: `url(${Book})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto work-div"
           >
             {/* Hover effect */}
             <div className="opacity-0 group-hover:opacity-100">
@@ -68,7 +69,7 @@ function Project() {
                 React JS Application
               </span>
               <div>
-                <a href="">
+                <a href="https://venkatamora.github.io/Book-and-Movies-library/index.html">
                   <button> Demo</button>
                 </a>
                 <a href="">
@@ -123,13 +124,19 @@ function Project() {
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto work-div"
           >
             {/* Hover effect */}
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl  font-serif tracking-wider">
-                React JS Application
+            <div className="opacity-0 group-hover:opacity-100 m-5">
+              <span className="text-2xl  font-serif tracking-wider ">
+                Weather Dashboard
               </span>
+              <p>
+                The App uses openweather map API to access weather data for a
+                particular city and a 5 day weather Prediction
+              </p>
               <div>
                 <a href=" https://rodiat-oluwaseun.github.io/weatherDashboard/">
-                  <button> Demo</button>
+                  <button>
+                    <FontAwesomeIcon icon="fal fa-link" />
+                  </button>
                 </a>
                 <a href="">
                   <button>Link</button>
